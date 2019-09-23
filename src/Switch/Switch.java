414f96ddc91553c8b70391ee282f5fda50e0280d
HFrejlev <henrikfrejlev@hotmail.com>
2019-09-23 15:16:21 +0200

    Added case with switch.

--- src/Switch/Switch.java
+++ src/Switch/Switch.java
@@ -2,24 +2,29 @@ package Switch;
 
 public class Switch {
 
-    public static void main(String[] args) {
+    //If/else/else if are all both great tools. However, they can be a lot of work for more conditions
+    //Switch is a good alternative if you wanna check if a certain condition is met.
 
-    }
+    //In this example we have the variable of 3.
+    /*Instead of typing if, else or else if a lot of times, we'll just use the switch method to evaluate
+      the value and see if it matches any of the cases */
 
-        //If/else/else if are all both great tools. However, they can be a lot of work for more conditions
-        //Switch.Switch is a good alternative if you wanna check if a certain condition is met.
+    public static void main (String[] args) {
 
-        //In this example
-
-        int rank = 45;
-
-       
-
-
-
-
-
-
-
+        int rank = 3;
+        switch (rank) {
+            case 1:
+                System.out.println("Amateur");
+                break;
+            case 2:
+                System.out.println("Seasoned amateur");
+                break;
+            case 3:
+                System.out.println("Getting there");
+                break;
+            case 4:
+                System.out.println("You are the man!");
+        }
     }
 }
+
